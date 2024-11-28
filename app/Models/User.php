ee18df5dbbc4e20fff60a0c3af042ba57e798608
priyambodo <maspriyambodo@gmail.com>
2024-11-28 14:08:37 +0700

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'is_trash',
+        'created_by',
+        'updated_by'
     ];
 
     /**
