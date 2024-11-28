79ef1aec68e4f3ca5a46010bb8c2457cc48042ee
priyambodo <maspriyambodo@gmail.com>
2024-11-28 21:48:39 +0700

    add migration sys_menu

---
+++ database/migrations/2024_11_28_214740_create_sys_menu_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sys_menu', function (Blueprint $table) {
+            $table->id(); // This will create an auto-incrementing id column
+            $table->integer('menu_parent')->nullable();
+            $table->string('nama', 50)->nullable()->charset('utf8mb4')->collation('utf8mb4_general_ci');
+            $table->string('link', 255)->nullable()->charset('utf8mb4')->collation('utf8mb4_general_ci');
+            $table->integer('order_no')->nullable();
+            $table->integer('group_menu')->nullable()->comment('1. applications\r\n2. report\r\n3. systems');
+            $table->string('icon', 50)->nullable()->charset('utf8mb4')->collation('utf8mb4_general_ci');
+            $table->string('description', 255)->nullable()->charset('utf8mb4')->collation('utf8mb4_bin');
+            $table->integer('is_trash')->default(0)->comment('0. aktif 1. deleted');
+            $table->integer('created_by')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            
+            $table->primary('id'); // Set the primary key
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sys_menu');
+    }
+};
