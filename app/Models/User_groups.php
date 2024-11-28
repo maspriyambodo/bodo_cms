ffd53111ef570789edfbc455a5fe8afc9686c0a1
priyambodo <maspriyambodo@gmail.com>
2024-11-28 22:01:12 +0700

    add module user_groups

---
+++ app/Models/User_groups.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+class User_groups extends Model {
+
+    use HasFactory,
+        Notifiable;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'is_trash',
+        'created_at',
+        'updated_at',
+        'created_by',
+        'updated_by'
+    ];
+}
