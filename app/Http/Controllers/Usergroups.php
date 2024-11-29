8c0fb7f24b092f454fa5973df4641b704043b3d2
priyambodo <maspriyambodo@gmail.com>
2024-11-29 17:19:55 +0700

    update usergroup module

--- app/Http/Controllers/Usergroups.php
+++ app/Http/Controllers/Usergroups.php
@@ -3,59 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User_groups;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\View\View;
 
-class Usergroups extends Controller
-{
-    public function index(Request $request): View
-    {
-        $user = Auth::user();
-//        dd($user->role);
-        $role_user = User_groups::where('id', $user->role)->first();
-        return view('usergroups.index', [
-            'user' => $request->user(),
-            'role_user' => $role_user
-        ]);
-    }
+class Usergroups extends Controller {
 
-    /**
-     * Update the user's profile information.
-     */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
-    {
-        $request->user()->fill($request->validated());
-
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
-        }
-
-        $request->user()->save();
-
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
-    }
-
-    /**
-     * Delete the user's account.
-     */
-    public function destroy(Request $request): RedirectResponse
-    {
-        $request->validateWithBag('userDeletion', [
-            'password' => ['required', 'current_password'],
-        ]);
-
-        $user = $request->user();
-
-        Auth::logout();
-
-        $user->delete();
-
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
-        return Redirect::to('/');
+    public function index(Request $request) {
+        return view('usergroups.index');
     }
 }
