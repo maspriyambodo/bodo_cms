3d1a3e7a66dc83dc30a1f3cc0f7316738b0e9494
priyambodo <maspriyambodo@gmail.com>
2024-11-29 17:18:13 +0700

    update menuController

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,18 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Models\User_groups;
-use App\Models\Menu;
 
 class MenuController extends Controller {
 
     public function index(Request $request) {
         return view('menu.index');
-    }
-
-    public function getMenu() {
-        $menus = Menu::with('children')->whereNull('menu_parent')->get();
-        return view('layouts.admin_template', compact('menus'));
     }
 }
