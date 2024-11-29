ac4373f426916f6415fd3464eab913ca99e3e729
priyambodo <maspriyambodo@gmail.com>
2024-11-29 17:16:23 +0700

    update menuController

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,25 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Menu;
-use App\Models\User_groups;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\View\View;
+use App\Models\User_groups;
+use App\Models\Menu;
 
 class MenuController extends Controller {
 
-    public function index(Request $request): View {
-        $user = Auth::user();
-//        dd($user->role);
-        $role_user = User_groups::where('id', $user->role)->first();
-        $menus = Menu::with('children')->whereNull('menu_parent')->get();
-        return view('menu.index', [
-            'user' => $request->user(),
-            'role_user' => $role_user,
-            'menus' => $menus
-        ]);
+    public function index(Request $request) {
+        return view('menu.index');
     }
 
     public function getMenu() {
