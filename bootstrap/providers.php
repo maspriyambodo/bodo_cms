d062bb4918a7e38c82ef6ffc332a3003305b1fa4
priyambodo <maspriyambodo@gmail.com>
2024-12-02 02:53:33 +0700

    update bootstrap provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,7 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\MenuServiceProvider::class,
     App\Providers\ViewServiceProvider::class,
+    Yajra\DataTables\ButtonsServiceProvider::class,
     Yajra\DataTables\DataTablesServiceProvider::class,
+    Yajra\DataTables\HtmlServiceProvider::class,
 ];
