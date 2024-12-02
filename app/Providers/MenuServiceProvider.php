f004d7d6fb7f73b10cee9abecbfb2072af061b55
priyambodo <maspriyambodo@gmail.com>
2024-12-02 10:06:02 +0700

    update menuserviceprovider

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -28,7 +28,7 @@ class MenuServiceProvider extends ServiceProvider {
                     $user = Auth::user();
                     $role_user = User_groups::find($user->role);
                     $menus = Menu::with(['children'])
-                            ->select('sys_menu.menu_parent', 'sys_menu.nama', 'sys_permissions.role_id', 'sys_permissions.v', 'sys_permissions.c', 'sys_permissions.r', 'sys_permissions.u', 'sys_permissions.d')
+                            ->select('sys_menu.menu_parent', 'sys_menu.link', 'sys_menu.nama', 'sys_permissions.role_id', 'sys_permissions.v', 'sys_permissions.c', 'sys_permissions.r', 'sys_permissions.u', 'sys_permissions.d')
                             ->join('sys_permissions', 'sys_menu.id', '=', 'sys_permissions.id_menu')
                             ->whereNull('menu_parent')
                             ->where(['is_trash' => 0, 'sys_permissions.v' => 1])
