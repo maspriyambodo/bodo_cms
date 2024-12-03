1894307acb14eaf82c2736fba0f3a450a0d9d63e
priyambodo <maspriyambodo@gmail.com>
2024-12-04 00:18:11 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,24 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
-    
+
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    
+
     Route::get('/menu', [MenuController::class, 'index'])->name('menu');
-    
+
     Route::get('/menu-group', [GroupMenu::class, 'index'])->name('menu-group');
-    
+
     Route::get('/parameter', [Parameter::class, 'index'])->name('parameter-system');
-    
+
     Route::get('/permission', [Permission::class, 'index'])->name('permission-system');
-    
+
     Route::get('/user-management', [UserController::class, 'index'])->name('user-management');
     Route::get('/user-management-json', [UserController::class, 'json'])->name('user-json');
     Route::get('/user-management-edit/{id}', [UserController::class, 'edit'])->name('user-edit');
-    
+    Route::post('/user-management-store', [UserController::class, 'store'])->name('user-store');
+
     Route::get('/user-groups', [Usergroups::class, 'index'])->name('user-groups');
-    
+
     Route::get('/menu', [MenuController::class, 'index'])->name('menu');
 });
 
