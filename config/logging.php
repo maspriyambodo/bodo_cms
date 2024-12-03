cceede58dcb0a2da5b85f503a92ed510cf4b7fe9
priyambodo <maspriyambodo@gmail.com>
2024-12-04 00:17:59 +0700

    update logging

--- config/logging.php
+++ config/logging.php
@@ -68,8 +68,8 @@ return [
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
-            'days' => env('LOG_DAILY_DAYS', 14),
+            'level' => env('LOG_LEVEL', 'error'),
+            'days' => env('LOG_DAILY_DAYS', 30),
             'replace_placeholders' => true,
         ],
 
