7fc0fa2555fac2ba821c3821d9ac0c279eca9589
priyambodo <maspriyambodo@gmail.com>
2024-12-05 17:25:19 +0700

    add filte by route

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Route;
 use App\Models\Permission as user_permission;
 
 class Controller extends BaseController {
@@ -13,10 +14,12 @@ class Controller extends BaseController {
         ValidatesRequests;
 
     public function permission_user() {
+        $menu_link = Route::currentRouteName();
         $exec = user_permission::select('sys_menu.nama AS nama_menu', 'sys_menu.link AS link_menu', 'sys_permissions.role_id', 'sys_permissions.id_menu', 'sys_permissions.v', 'sys_permissions.c', 'sys_permissions.r', 'sys_permissions.u', 'sys_permissions.d')
                 ->join('users', 'sys_permissions.role_id', '=', 'users.role')
                 ->join('sys_menu', 'sys_permissions.id_menu', '=', 'sys_menu.id')
                 ->where('users.role', auth()->user()->role)
+                ->where('sys_menu.link', $menu_link)
                 ->get();
         $data = [];
         foreach ($exec as $dt_user) {
