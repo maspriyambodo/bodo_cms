c1034c8e493f12ce45d790b4359d352c9795e7be
priyambodo <maspriyambodo@gmail.com>
2024-12-06 01:59:06 +0700

    remove duplicate dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
