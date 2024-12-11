976c42c4b1d8997c72f67f08547de5bb13d1c4b8
priyambodo <maspriyambodo@gmail.com>
2024-12-11 17:29:12 +0700

    update permission view

--- resources/views/permission/index.blade.php
+++ resources/views/permission/index.blade.php
@@ -1,1 +1,87 @@
 @extends('layouts.admin_template')
+@push('stylesheet')
+<link href="{{ asset('src/plugins/custom/prismjs/prismjs.bundle.css'); }}" rel="stylesheet" type="text/css" />
+
+@endpush
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <div class="row clearfix">
+            <div class="col-md-4 my-2">
+                <input id="keyword" name="keyword" type="text" class="form-control" placeholder="search data..."/>
+            </div>
+            <div class="col-md-8 text-end my-2">
+                <div class="btn-group" role="group" aria-label="action button">
+                    <!--begin::Menu wrapper-->
+                    <div>
+                        <!--begin::Toggle-->
+                        <button type="button" class="btn btn-secondary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start">
+                            show data
+                            <span class="svg-icon fs-3 rotate-180 ms-3 me-0"><i class="bi bi-caret-down-fill"></i></span>
+                        </button>
+                        <!--end::Toggle-->
+
+                        <!--begin::Menu-->
+                        <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-auto min-w-200 mw-300px" data-kt-menu="true">
+                            <!--begin::Menu item-->
+                            <div class="menu-item px-3">
+                                <div class="menu-content fs-6 text-gray-900 fw-bold px-3 py-4"></div>
+                            </div>
+                            <!--end::Menu item-->
+
+                            <!--begin::Menu separator-->
+                            <div class="separator mb-3 opacity-75"></div>
+                            <!--end::Menu separator-->
+
+                            <!--begin::Menu item-->
+                            <div class="menu-item px-3">
+                                <a id="_10" href="javascript:void(0);" class="menu-link px-3">
+                                    show 10
+                                </a>
+                            </div>
+                            <div class="menu-item px-3">
+                                <a id="_25" href="javascript:void(0);" class="menu-link px-3">
+                                    show 25
+                                </a>
+                            </div>
+                            <div class="menu-item px-3">
+                                <a id="_50" href="javascript:void(0);" class="menu-link px-3">
+                                    show 50
+                                </a>
+                            </div>
+                            <div class="menu-item px-3">
+                                <a id="_100" href="javascript:void(0);" class="menu-link px-3">
+                                    show 100
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <button id="dt_reload" type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="reload data" data-kt-initialized="1"><i class="bi bi-arrow-clockwise"></i></button>
+                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addModal"><i class="bi bi-plus-lg" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="add new data" data-kt-initialized="1"></i></button>
+                </div>
+            </div>
+        </div>
+        <div class="table-responsive">
+            <table id="table-permission" class="table table-rounded table-striped table-hover gy-5 gs-7 border my-5">
+                <thead>
+                    <tr class="bg-light fw-bold fs-6 border-bottom-2 border-gray-200 text-center border">
+                        <th>No</th>
+                        <th>#</th>
+                        <th>Parent</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Status</th>
+                        <th>Register Date</th>
+                    </tr>
+                </thead>
+            </table>
+            <div class="clearfix my-5"></div>
+        </div>
+    </div>
+</div>
+@endsection
+@push('scripts')
+<script src="{{ asset('src/plugins/custom/prismjs/prismjs.bundle.js'); }}"></script>
+<script src="{{ asset('src/plugins/custom/datatables/datatables.bundle.js'); }}"></script>
+@include('permission.table_config')
+@endpush
---
+++ resources/views/permission/table_config.blade.php
@@ -0,0 +1,85 @@
+<script>
+    var dt;
+    var KTDatatablesServerSide = function () {
+        var initDatatable = function () {
+            var dt = $('#table-permission').DataTable({
+                searchDelay: 500,
+                serverSide: true,
+                paging: true,
+                deferRender: true,
+                info: true,
+                stateSave: true,
+                ajax: {
+                    url: "permission-json",
+                    data: function (d) {
+                        d.keyword = $("#keyword").val();
+                    }
+                },
+                columns: [{
+                        orderable: false,
+                        className: "text-center",
+                        render: function (data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    }, {
+                        data: "button",
+                        className: "text-center",
+                        orderable: false
+                    }, {
+                        data: "name"
+                    }, {
+                        data: "children",
+                        render: function (data) {
+                            var child_name = '';
+                            for (var i = 0, l = data.length; i < l; i++) {
+                                child_name = data[i].name;
+                            }
+                            return child_name;
+                        }
+                    }, {
+                        data: "description"
+                    }, {
+                        data: "status_aktif"
+                    }, {
+                        data: "created_at",
+                        className: 'text-center'
+                    }],
+                displayStart: 0,
+                pageLength: 10,
+                rowCallback: function (row, data) {
+                    $('.dataTables_length').remove();
+                    $(row).addClass('border');
+                }
+            });
+            $('#_10').on('click', function () {
+                dt.page.len(10).draw();
+            });
+            $('#_25').on('click', function () {
+                dt.page.len(25).draw();
+            });
+            $('#_50').on('click', function () {
+                dt.page.len(50).draw();
+            });
+            $('#_100').on('click', function () {
+                dt.page.len(100).draw();
+            });
+            $('#dt_reload').on('click', function () {
+                $('#table-permission').DataTable().ajax.reload();
+            });
+            $('#keyword').on('keyup', function () {
+                dt.search(this.value).draw();
+            });
+            dt.on('draw', function () {
+                KTMenu.createInstances();
+            });
+        };
+        return {
+            init: function () {
+                initDatatable();
+            }
+        };
+    }();
+    KTUtil.onDOMContentLoaded(function () {
+        KTDatatablesServerSide.init();
+    });
+</script>
