ada6afc26d972159d38fe471af7b3e4ff5e49078
priyambodo <maspriyambodo@gmail.com>
2024-12-11 17:32:20 +0700

    update view users

--- resources/views/users/table_user.blade.php
+++ resources/views/users/table_user.blade.php
@@ -1,79 +1,73 @@
 <script>
     var dt;
     var KTDatatablesServerSide = function () {
-    var initDatatable = function () {
-    var dt = $('#table-user').DataTable({
-    searchDelay: 500,
-            serverSide: true,
-            paging: true,
-            deferRender: true,
-            info: true,
-            stateSave: true,
-            layout: {
-            topStart: {
-            buttons: [
-                    'pageLength',
-            {
-            titleAttr: 'refresh data',
-                    text: '<i class="bi bi-arrow-clockwise"></i>',
-                    action: function () {
-                    $('#table-user').DataTable().ajax.reload();
-                    }
-            },
-                    @if ($user_access['create'])
-            {
-            titleAttr: 'add new user',
-                    text: '<i class="bi bi-person-plus-fill"></i>',
-                    attr: {
-                    "data-bs-toggle": 'modal',
-                            "data-bs-target": '#addModal'
-                    }
-            }
-            @endif
-            ]
-            }
-            },
-            ajax: {
-            url: "user-management-json",
+        var initDatatable = function () {
+            var dt = $('#table-user').DataTable({
+                searchDelay: 500,
+                serverSide: true,
+                paging: true,
+                deferRender: true,
+                info: true,
+                stateSave: true,
+                ajax: {
+                    url: "user-management-json",
                     data: function (d) {
-                    d.keyword = $("#keyword").val();
+                        d.keyword = $("#keyword").val();
                     }
-            },
-            columnDefs: [
-            {orderable: false, targets: []},
-            ],
-            columns: [
-            {
-            data: 'no_urut',
-                    className: "text-center",
-                    orderable: false
-            },
-            {data: "button", className: "text-center", orderable: false},
-            {data: "picture", className: "text-center", orderable: false},
-            {data: "name"},
-            {data: "email"},
-            {data: "role_name"},
-            {data: "status_aktif"},
-            {data: "created_at", className: 'text-center'}
-            ],
-            displayStart: 0,
-            pageLength: 10,
-            rowCallback: function (row, data) {
-            $(row).addClass('border');
-            },
-    });
-    dt.on('draw', function () {
-    KTMenu.createInstances();
-    });
-    };
-    return {
-    init: function () {
-    initDatatable();
-    }
-    };
+                },
+                columnDefs: [
+                    {orderable: false, targets: []},
+                ],
+                columns: [
+                    {
+                        data: 'no_urut',
+                        className: "text-center",
+                        orderable: false
+                    },
+                    {data: "button", className: "text-center", orderable: false},
+                    {data: "picture", className: "text-center", orderable: false},
+                    {data: "name"},
+                    {data: "email"},
+                    {data: "role_name"},
+                    {data: "status_aktif"},
+                    {data: "created_at", className: 'text-center'}
+                ],
+                displayStart: 0,
+                pageLength: 10,
+                rowCallback: function (row, data) {
+                    $(row).addClass('border');
+                    $('.dataTables_length').remove();
+                },
+            });
+            $('#_10').on('click', function () {
+                dt.page.len(10).draw();
+            });
+            $('#_25').on('click', function () {
+                dt.page.len(25).draw();
+            });
+            $('#_50').on('click', function () {
+                dt.page.len(50).draw();
+            });
+            $('#_100').on('click', function () {
+                dt.page.len(100).draw();
+            });
+            $('#dt_reload').on('click', function () {
+                $('#table-user').DataTable().ajax.reload();
+            });
+            $('#keyword').on('keyup', function () {
+                dt.search(this.value).draw();
+            });
+            dt.on('draw', function () {
+                KTMenu.createInstances();
+            });
+        };
+        return {
+            init: function () {
+                initDatatable();
+            }
+        };
     }();
-// On document ready
     KTUtil.onDOMContentLoaded(function () {
-    KTDatatablesServerSide.init();
+        KTDatatablesServerSide.init();
     });
 </script>
