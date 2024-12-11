52c34b97ada8f226f937a993d40586a8512caf97
priyambodo <maspriyambodo@gmail.com>
2024-12-11 19:03:33 +0700

    update permission view

--- resources/views/permission/index.blade.php
+++ resources/views/permission/index.blade.php
@@ -12,28 +12,16 @@
             </div>
             <div class="col-md-8 text-end my-2">
                 <div class="btn-group" role="group" aria-label="action button">
-                    <!--begin::Menu wrapper-->
                     <div>
-                        <!--begin::Toggle-->
                         <button type="button" class="btn btn-secondary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start">
                             show data
                             <span class="svg-icon fs-3 rotate-180 ms-3 me-0"><i class="bi bi-caret-down-fill"></i></span>
                         </button>
-                        <!--end::Toggle-->
-
-                        <!--begin::Menu-->
                         <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-auto min-w-200 mw-300px" data-kt-menu="true">
-                            <!--begin::Menu item-->
                             <div class="menu-item px-3">
                                 <div class="menu-content fs-6 text-gray-900 fw-bold px-3 py-4"></div>
                             </div>
-                            <!--end::Menu item-->
-
-                            <!--begin::Menu separator-->
                             <div class="separator mb-3 opacity-75"></div>
-                            <!--end::Menu separator-->
-
-                            <!--begin::Menu item-->
                             <div class="menu-item px-3">
                                 <a id="_10" href="javascript:void(0);" class="menu-link px-3">
                                     show 10
@@ -79,6 +67,17 @@
         </div>
     </div>
 </div>
+@if($user_access['create'])
+@include('permission.add_permission')
+@endif
+@if($user_access['update'])
+@include('permission.edit_permission')
+@endif
+@if($user_access['delete'])
+@include('permission.delete_permission')
+@include('permission.restore_permission')
+@include('permission.setting_permission')
+@endif
 @endsection
 @push('scripts')
 <script src="{{ asset('src/plugins/custom/prismjs/prismjs.bundle.js'); }}"></script>
