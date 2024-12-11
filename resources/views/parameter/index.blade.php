e22b349817952ac69ea03eb883720b01dee7ec09
priyambodo <maspriyambodo@gmail.com>
2024-12-11 18:12:58 +0700

    update parameter view

--- resources/views/parameter/index.blade.php
+++ resources/views/parameter/index.blade.php
@@ -6,17 +6,66 @@
 @section('content')
 <div class="card">
     <div class="card-body">
+        <div class="row clearfix">
+            @if($user_access['read'])
+            <div class="col-md-4 my-2">
+                <input id="keyword" name="keyword" type="text" class="form-control" placeholder="search data..."/>
+            </div>
+            @endif
+            <div class="col-md-8 text-end my-2">
+                <div class="btn-group" role="group" aria-label="action button">
+                    @if($user_access['read'])
+                    <div>
+                        <button type="button" class="btn btn-secondary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start">
+                            show data
+                            <span class="svg-icon fs-3 rotate-180 ms-3 me-0"><i class="bi bi-caret-down-fill"></i></span>
+                        </button>
+                        <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-auto min-w-200 mw-300px" data-kt-menu="true">
+                            <div class="menu-item px-3">
+                                <div class="menu-content fs-6 text-gray-900 fw-bold px-3 py-4"></div>
+                            </div>
+                            <div class="separator mb-3 opacity-75"></div>
+                            <div class="menu-item px-3">
+                                <a id="_10" href="javascript:void(0);" class="menu-link px-3">
+                                    show 10
+                                </a>
+                            </div>
+                            <div class="menu-item px-3">
+                                <a id="_25" href="javascript:void(0);" class="menu-link px-3">
+                                    show 25
+                                </a>
+                            </div>
+                            <div class="menu-item px-3">
+                                <a id="_50" href="javascript:void(0);" class="menu-link px-3">
+                                    show 50
+                                </a>
+                            </div>
+                            <div class="menu-item px-3">
+                                <a id="_100" href="javascript:void(0);" class="menu-link px-3">
+                                    show 100
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <button id="dt_reload" type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="reload data" data-kt-initialized="1"><i class="bi bi-arrow-clockwise"></i></button>
+                    @endif
+                    @if($user_access['create'])
+                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addModal"><i class="bi bi-plus-lg" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="add new data" data-kt-initialized="1"></i></button>
+                    @endif
+                </div>
+            </div>
+        </div>
         <div class="table-responsive py-5">
-            <table id="table-parameter" class="table table-rounded table-striped table-hover gy-5 gs-7 border" style="width: 100%;">
+            <table id="table-parameter" class="table table-rounded table-striped table-hover gy-5 gs-7 border">
                 <thead>
                     <tr class="bg-light fw-bold fs-6 border-bottom-2 border-gray-200 text-center border">
-                        <th>#</th>
-                        <th>ID</th>
-                        <th>GROUP</th>
-                        <th>VALUE</th>
-                        <th>DESCRIPTION</th>
-                        <th>Status</th>
-                        <th>Register Date</th>
+                        <th class="text-center">#</th>
+                        <th class="text-center">ID</th>
+                        <th class="text-center">GROUP</th>
+                        <th class="text-center">VALUE</th>
+                        <th class="text-center">DESCRIPTION</th>
+                        <th class="text-center">Status</th>
+                        <th class="text-center">Register Date</th>
                     </tr>
                 </thead>
             </table>
--- resources/views/parameter/table_parameter.blade.php
+++ resources/views/parameter/table_parameter.blade.php
@@ -1,81 +1,79 @@
 <script>
-  var dt;
-  var KTDatatablesServerSide = function() {
-    var initDatatable = function() {
-      var dt = $('#table-parameter').DataTable({
-        searchDelay: 500,
-        serverSide: true,
-        paging: true,
-        deferRender: true,
-        info: true,
-        stateSave: true,
-        layout: {
-          topStart: {
-            buttons: ['pageLength', {
-                titleAttr: 'refresh data',
-                text: '<i class="bi bi-arrow-clockwise"></i>',
-                action: function() {
-                  $('#table-parameter').DataTable().ajax.reload();
+    var dt;
+    var KTDatatablesServerSide = function () {
+        var initDatatable = function () {
+            var dt = $('#table-parameter').DataTable({
+                searchDelay: 500,
+                serverSide: true,
+                paging: true,
+                deferRender: true,
+                info: true,
+                stateSave: true,
+                ajax: {
+                    url: "parameter-json",
+                    data: function (d) {
+                        d.keyword = $("#keyword").val();
+                    }
+                },
+                columnDefs: [{
+                        orderable: false,
+                        targets: [0, 1, 4]
+                    }],
+                columns: [{
+                        data: "button",
+                        className: "text-center",
+                        orderable: false
+                    }, {
+                        data: "id_param"
+                    }, {
+                        data: "param_group"
+                    }, {
+                        data: "param_value"
+                    }, {
+                        data: "param_desc"
+                    }, {
+                        data: "status_aktif"
+                    }, {
+                        data: "created_at",
+                        className: 'text-center'
+                    }],
+                displayStart: 0,
+                pageLength: 10,
+                rowCallback: function (row, data) {
+                    $('.dataTables_length').remove();
+                    $(row).addClass('border');
                 }
-              },
-              @if($access_user['create']) {
-                titleAttr: 'add new parameter',
-                text: ' <i class="bi bi-plus"></i>',
-                attr: {
-                  "data-bs-toggle": 'modal',
-                  "data-bs-target": '#addModal'
-                }
-              }
-              @endif
-            ]
-          }
-        },
-        ajax: {
-          url: "parameter-json",
-          data: function(d) {
-            d.keyword = $("#keyword").val();
-          }
-        },
-        columnDefs: [{
-          orderable: false,
-          targets: []
-        }, ],
-        columns: [{
-          data: "button",
-          className: "text-center",
-          orderable: false
-        }, {
-          data: "id_param"
-        }, {
-          data: "param_group"
-        }, {
-          data: "param_value"
-        }, {
-          data: "param_desc"
-        }, {
-          data: "status_aktif"
-        }, {
-          data: "created_at",
-          className: 'text-center'
-        }],
-        displayStart: 0,
-        pageLength: 10,
-        rowCallback: function(row, data) {
-          $(row).addClass('border');
-        },
-      });
-      dt.on('draw', function() {
-        KTMenu.createInstances();
-      });
-    };
-    return {
-      init: function() {
-        initDatatable();
-      }
-    };
-  }();
-  // On document ready
-  KTUtil.onDOMContentLoaded(function() {
-    KTDatatablesServerSide.init();
-  });
+            });
+            $('#_10').on('click', function () {
+                dt.page.len(10).draw();
+            });
+            $('#_25').on('click', function () {
+                dt.page.len(25).draw();
+            });
+            $('#_50').on('click', function () {
+                dt.page.len(50).draw();
+            });
+            $('#_100').on('click', function () {
+                dt.page.len(100).draw();
+            });
+            $('#dt_reload').on('click', function () {
+                $('#table-permission').DataTable().ajax.reload();
+            });
+            $('#keyword').on('keyup', function () {
+                dt.search(this.value).draw();
+            });
+            dt.on('draw', function () {
+                KTMenu.createInstances();
+            });
+        };
+        return {
+            init: function () {
+                initDatatable();
+            }
+        };
+    }();
+    // On document ready
+    KTUtil.onDOMContentLoaded(function () {
+        KTDatatablesServerSide.init();
+    });
 </script>
