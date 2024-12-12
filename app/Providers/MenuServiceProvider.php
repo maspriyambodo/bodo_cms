6cc7774e6520b8f629bfceb1440f9a1df0fd995a
priyambodo <maspriyambodo@gmail.com>
2024-12-12 13:44:34 +0700

    add filter query

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -26,13 +26,13 @@ class MenuServiceProvider extends ServiceProvider {
             try {
                 if (Auth::check()) {
                     $user = Auth::user();
-                    
+
                     $role_user = User_groups::find($user->role);
                     $menus = Menu::with(['children'])
                             ->select('sys_menu.menu_parent', 'sys_menu.link', 'sys_menu.nama', 'sys_permissions.role_id', 'sys_permissions.v', 'sys_permissions.c', 'sys_permissions.r', 'sys_permissions.u', 'sys_permissions.d')
                             ->join('sys_permissions', 'sys_menu.id', '=', 'sys_permissions.id_menu')
                             ->whereNull('menu_parent')
-                            ->where(['sys_menu.is_trash' => 0, 'sys_permissions.v' => 1, 'sys_permissions.role_id' => $user->role])
+                            ->where(['sys_menu.is_trash' => 0, 'sys_permissions.is_trash' => 0, 'sys_permissions.v' => 1, 'sys_permissions.role_id' => $user->role])
                             ->get();
                     $menugroup = MenuGroup::where(['is_trash' => 0])->get();
                     $view->with(compact('user', 'role_user', 'menus', 'menugroup'));
