58a7a68eee3c707ba812bbcf1a14e78b1c9bcdfd
maspriyambodo <maspriyambodo@gmail.com>
2024-12-12 23:05:36 +0700

    resolve error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder {
 
     /**
@@ -13,5 +14,10 @@ class DatabaseSeeder extends Seeder {
      */
     public function run(): void {
         $this->call(UserSeeder::class);
+        $this->call(SysMenuGroupSeeder::class);
+        $this->call(SysMenuSeeder::class);
+        $this->call(SysParamSeeder::class);
+        $this->call(SysPermissionsSeeder::class);
+        $this->call(UserGroupsSeeder::class);
     }
 }
--- database/seeders/UserGroupsSeeder.php
+++ database/seeders/UserGroupsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserGroupsSeeder extends Seeder {
 
@@ -20,7 +21,7 @@ class UserGroupsSeeder extends Seeder {
             ['id' => 6, 'name' => 'user-level-6', 'description' => 'user-level-6', 'is_trash' => 0, 'created_at' => '2024-11-28 22:21:59', 'created_by' => 1, 'updated_at' => '2024-11-28 22:22:06', 'updated_by' => 1],
             ['id' => 7, 'name' => 'user-level-7', 'description' => 'user-level-7', 'is_trash' => 0, 'created_at' => '2024-11-28 22:21:59', 'created_by' => 1, 'updated_at' => '2024-11-28 22:22:06', 'updated_by' => 1],
             ['id' => 8, 'name' => 'user-level-8', 'description' => 'user-level-8', 'is_trash' => 0, 'created_at' => '2024-11-28 22:21:59', 'created_by' => 1, 'updated_at' => '2024-11-28 22:22:06', 'updated_by' => 1],
-            ['id' => 9, 'name' => 'root', 'description' => 'super user level', 'is_trash' => 0, 'created_at' => '2024-11-28 22:21:59', 'created_by' => 1, 'updated_at' => '2024- 11-28 22:22:06', 'updated_by' => 1],
+            ['id' => 9, 'name' => 'root', 'description' => 'super user level', 'is_trash' => 0, 'created_at' => '2024-11-28 22:21:59', 'created_by' => 1, 'updated_at' => '2024-11-28 22:22:06', 'updated_by' => 1],
         ]);
     }
 }
