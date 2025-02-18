30d3c46f0e4e80fd4178471d7cd6c62c7a7aceed
priyambodo <maspriyambodo@gmail.com>
2025-02-18 12:35:47 +0700

    add module group-menu

---
+++ resources/views/grupmenu/table_grupmenu.blade.php
@@ -0,0 +1,91 @@
+<script>
+    var dt;
+    var KTDatatablesServerSide = function () {
+        var initDatatable = function () {
+            var dt = $('#table-menu').DataTable({
+                searchDelay: 500,
+                serverSide: true,
+                paging: true,
+                deferRender: true,
+                info: true,
+                stateSave: true,
+                ajax: {
+                    url: "menugrup-json",
+                    data: function (d) {
+                        d.keyword = $("#keyword").val();
+                    }
+                },
+                columnDefs: [
+                    {
+                        orderable: false, targets: [0, 1, 3]
+
+                    }
+                ],
+                columns: [
+                    {
+                        className: "text-center",
+                        render: function (data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    }, 
+                    {
+                        data: "button",
+                        className: "text-center"
+
+                    },
+                    {
+                        data: "nama"
+                    }, 
+                    {
+                        data: "description"
+                    },
+                    {
+                        data: "order_no",
+                        className: "text-center"
+                    },
+                    {
+                        data: "status_aktif"
+                    },
+                    {
+                        data: "created_at"
+                    }
+                ],
+                displayStart: 0,
+                pageLength: 10,
+                rowCallback: function (row, data) {
+                    $('.dataTables_length').remove();
+                    $(row).addClass('border');
+                }
+            });
+            $('#_10').on('click', function () {
+                dt.page.len(10).draw();
+            });
+            $('#_25').on('click', function () {
+                dt.page.len(25).draw();
+            });
+            $('#_50').on('click', function () {
+                dt.page.len(50).draw();
+            });
+            $('#_100').on('click', function () {
+                dt.page.len(100).draw();
+            });
+            $('#dt_reload').on('click', function () {
+                $('#table-menu').DataTable().ajax.reload();
+            });
+            $('#keyword').on('keyup', function () {
+                dt.search(this.value).draw();
+            });
+            dt.on('draw', function () {
+                KTMenu.createInstances();
+            });
+        };
+        return {
+            init: function () {
+                initDatatable();
+            }
+        };
+    }();
+    KTUtil.onDOMContentLoaded(function () {
+        KTDatatablesServerSide.init();
+    });
+</script>
