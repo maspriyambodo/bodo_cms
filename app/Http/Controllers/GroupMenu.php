bdc142d47180b737a8b62589f3ac98e28c58b690
priyambodo <maspriyambodo@gmail.com>
2025-02-18 12:36:11 +0700

    add module group-menu

--- app/Http/Controllers/GroupMenu.php
+++ app/Http/Controllers/GroupMenu.php
@@ -3,10 +3,95 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
+use App\Models\Permission as db_permission;
+use App\Models\Parameter as ParameterModel;
+use App\Models\User_groups;
+use App\Models\MenuGroup;
+use Yajra\DataTables\Facades\DataTables;
 
-class GroupMenu extends Controller
-{
+class GroupMenu extends Controller {
+
+    private function user_permission() {
+        return Controller::permission_user();
+    }
+
     public function index(Request $request) {
-        return view('grupmenu.index');
+        $user_access = $this->user_permission();
+        $menu_group = MenuGroup::where('is_trash', 0)->get();
+        return view('grupmenu.index', compact('user_access', 'menu_group'));
+    }
+
+    public function json(Request $request) {
+        if (!$this->user_permission()['read']) {
+            return [
+                'draw' => 0,
+                'recordsTotal' => 0,
+                'recordsFiltered' => 0,
+                'data' => []
+            ];
+        }
+        $exec = MenuGroup::orderBy('id', 'asc');
+        $this->applyFilters($exec, $request);
+        $dt_param = $exec->get();
+        return Datatables::of($dt_param)
+                        ->editColumn('created_at', fn($row) => date('d M Y', strtotime($row->created_at)))
+                        ->addColumn('status_aktif', fn($row) => $row->is_trash == 0 ? "<span class=\"badge badge-success w-100\">aktif</span>" : "<span class=\"badge badge-light-dark w-100\">deleted</span>")
+                        ->addColumn('button', fn($row) => $this->getActionButtons($row))
+                        ->rawColumns(['status_aktif', 'button'])
+                        ->make(true);
+    }
+
+    private function applyFilters($query, Request $request) {
+        if ($request->filled('keyword')) {
+            $query->where(function ($q) use ($request) {
+                $q->where('nama', 'like', "%" . $request->keyword . "%")
+                        ->orWhere('description', 'like', "%" . $request->keyword . "%");
+            });
+        }
+    }
+
+    private function getActionButtons($row) {
+        $permissions = $this->user_permission();
+        $canUpdate = $permissions['update'];
+        $canDelete = $permissions['delete'];
+        if (!$canUpdate && !$canDelete) {
+            return '';
+        }
+        $buttons = '<a type="button" class="btn btn-secondary btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="right-start">
+            <i class="bi bi-three-dots-vertical"></i>
+        </a><div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-200px py-4" data-kt-menu="true"><div class="menu-item px-3">
+                <span class="text-center text-muted py-3 mb-2">' . $row->name . ' Action</span>
+            </div>';
+
+        if ($canUpdate) {
+            $buttons .= '<div class="menu-item px-3 border">
+                <a href="javascript:void(0);" class="menu-link px-3" onclick="editData(&apos;' . $row->id . '&apos;);">
+                    <i class="bi bi-pencil-square text-warning mx-2"></i> Edit
+                </a>
+            </div>';
+        }
+
+        if ($canDelete && $row->is_trash == 0) {
+            if ($row->is_trash == 0) {
+                $buttons .= '<div class="menu-item px-3 border">
+                <a href="javascript:void(0);" class="menu-link px-3" onclick="deleteData(&apos;' . $row->id . '&apos;);">
+                    <i class="bi bi-trash text-danger mx-2"></i> Delete
+                </a>
+            </div>';
+            } else {
+                $buttons .= '<div class="menu-item px-3 border">
+                <a href="javascript:void(0);" class="menu-link px-3" onclick="restoreData(&apos;' . $row->id . '&apos;);">
+                    <i class="bi bi-recycle text-success mx-2"></i> Activate
+                </a>
+            </div>';
+            }
+        }
+
+        $buttons .= "</div>";
+
+        return $buttons;
     }
 }
