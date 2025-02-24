1cc340baada5afe3fb815c917442c2174f85ff5d
priyambodo <maspriyambodo@gmail.com>
2025-02-24 11:12:41 +0700

    update menu template

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -28,17 +28,18 @@ class MenuServiceProvider extends ServiceProvider {
                     $user = Auth::user();
 
                     $role_user = User_groups::find($user->role);
-                    $menus = Menu::with(['children', 'permissions' => function ($query) use ($user) {
+                    $menus = Menu::with(['children', 'group', 'permissions' => function ($query) use ($user) {
                                     $query->where('is_trash', 0)
                                             ->where('v', 1)
                                             ->where('role_id', $user->role);
                                 }])
                             ->whereNull('menu_parent')
                             ->where('is_trash', 0)
+                            ->orderBy('order_no', 'asc')
+                            ->orderBy('group_menu', 'asc')
                             ->get();
-                    
-                    $menugroup = MenuGroup::where(['is_trash' => 0])->get();
-                    $view->with(compact('user', 'role_user', 'menus', 'menugroup'));
+//                    dd($menus);
+                    $view->with(compact('user', 'role_user', 'menus'));
                 } else {
                     $view->with(['user' => null, 'role_user' => null, 'menus' => collect(), 'menugroup' => null]);
                 }
