97c60424219d76769f0172c07520f9c338a4020f
maspriyambodo <maspriyambodo@gmail.com>
2025-02-25 20:06:23 +0700

    update model provinsi

--- app/Models/MtProvinsi.php
+++ app/Models/MtProvinsi.php
@@ -10,12 +10,19 @@ class MtProvinsi extends Model {
     use HasFactory;
 
     protected $table = 'mt_provinsi';
+    protected $primaryKey = 'id_provinsi'; // Specify the primary key
     protected $fillable = [
+        'id_provinsi',
         'nama',
         'is_trash',
         'latitude',
         'longitude',
         'created_by',
         'updated_by',
+    ];
+    protected $casts = [
+        'latitude' => 'double',
+        'longitude' => 'double',
+        'is_trash' => 'integer',
     ];
 }
