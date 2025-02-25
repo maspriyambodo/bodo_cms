708b256056c7e1bcda84dad8054fdcbfe4399d6f
priyambodo <maspriyambodo@gmail.com>
2025-02-25 12:40:11 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ Route::middleware(['auth', 'verified'])
             Route::get('/', [Provinsi::class, 'index'])->name('provinsi');
             Route::get('/json', [Provinsi::class, 'json'])->name('provinsi');
             Route::post('/store', [Provinsi::class, 'store'])->name('provinsi');
+            Route::get('/edit/{id}', [Provinsi::class, 'edit'])->name('provinsi');
         });
 
 Route::middleware(['auth', 'verified'])
