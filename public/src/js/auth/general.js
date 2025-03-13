9addf30860566a03c052b16f337a2037646f6041
priyambodo <maspriyambodo@gmail.com>
2025-03-14 03:45:22 +0700

    update assets

--- public/src/js/auth/general.js
+++ public/src/js/auth/general.js
@@ -34,24 +34,6 @@ var KTSigninGeneral = (function () {
         });
     }
 
-    function showSuccessMessage() {
-        Swal.fire({
-            text: "You have successfully logged in!",
-            icon: "success",
-            buttonsStyling: false,
-            confirmButtonText: "Ok, got it!",
-            allowOutsideClick: false,
-            customClass: {confirmButton: "btn btn-primary"}
-        }).then(function (result) {
-            if (result.isConfirmed) {
-                form.reset();
-                var redirectUrl = form.getAttribute("data-kt-redirect-url");
-                if (redirectUrl)
-                    location.href = redirectUrl;
-            }
-        });
-    }
-
     function showErrorMessage(message) {
         Swal.fire({
             text: message,
@@ -67,42 +49,13 @@ var KTSigninGeneral = (function () {
 
     function handleSubmit(event) {
         event.preventDefault();
-
+        
         validation.validate().then(function (status) {
             if (status === "Valid") {
                 submitButton.setAttribute("data-kt-indicator", "on");
                 submitButton.disabled = true;
-
-                var formAction = form.getAttribute("action");
-                var isExternalURL = (() => {
-                    try {
-                        return new URL(formAction), true;
-                    } catch {
-                        return false;
-                    }
-                })();
-
-                if (isExternalURL) {
-                    axios.post(formAction, new FormData(form))
-                            .then(response => {
-                                if (response)
-                                    showSuccessMessage();
-                                else
-                                    showErrorMessage(response);
-                            })
-                            .catch(err => {
-                                var errmessage = err.toJSON();
-                                showErrorMessage(err.response.data.message + ", " + errmessage.message);
-                            })
-                            .finally(() => {
-                                submitButton.removeAttribute("data-kt-indicator");
-                                submitButton.disabled = false;
-                            });
-                } else {
-                    submitButton.removeAttribute("data-kt-indicator");
-                    submitButton.disabled = false;
-                    showSuccessMessage();
-                }
+                document.getElementById('overlay').style.display = 'flex';
+                
             } else {
                 showErrorMessage();
             }
