5e745d3bc70a8590d7040569c38f00b7e99531a7
priyambodo <maspriyambodo@gmail.com>
2025-03-19 01:50:28 +0700

    update page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,12 +3,13 @@
 <html data-bs-theme="dark">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <title>page not found</title>
+        <title>404 - Page Not Found</title>
     </head>
-    <body>
+    <body class="d-flex align-items-center justify-content-center vh-100">
         <div class="container">
-            <div style="margin-top: 15%;" class="text-center">
+            <div class="text-center">
                 <h1 class="display-1 fw-bold text-danger">404</h1>
                 <h2 class="fs-3">Oops! Page Not Found</h2>
                 <p class="lead text-muted">The page you're looking for doesn't exist or was moved.</p>
