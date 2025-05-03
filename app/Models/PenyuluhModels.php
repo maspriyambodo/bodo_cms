9769d854cb92ad319698136df459daa43331ac6b
priyambodo <maspriyambodo@gmail.com>
2025-05-03 14:12:22 +0700

    add models penyuluh

---
+++ app/Models/PenyuluhModels.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Penyuluh extends Model
+{
+    use HasFactory;
+
+    protected $table = 'dt_penyuluh';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'urut',
+        'nip',
+        'nipa',
+        'password',
+        'nik',
+        'nama',
+        'agama',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'alamat',
+        'tugas_provinsi',
+        'tugas_kabupaten',
+        'tugas_kecamatan',
+        'tugas_kua',
+        'status_pegawai',
+        'no_hp',
+        'email',
+        'pendidikan',
+        'jurusan',
+        'organisasi',
+        'spesialisasi',
+        'photo',
+        'swafoto',
+        'status',
+        'aktivasi',
+        'verifikator',
+        'created_date',
+        'approved_date',
+    ];
+
+    protected $casts = [
+        'tanggal_lahir' => 'date',
+        'created_date' => 'datetime',
+        'approved_date' => 'datetime',
+    ];
+
+    /**
+     * Get the provinsi related to the penyuluh.
+     */
+    public function provinsi()
+    {
+        return $this->belongsTo(MtProvinsi::class, 'tugas_provinsi', 'id_provinsi');
+    }
+
+    /**
+     * Get the kabupaten related to the penyuluh.
+     */
+    public function kabupaten()
+    {
+        return $this->belongsTo(MtKabupaten::class, 'tugas_kabupaten', 'id_kabupaten');
+    }
+
+    /**
+     * Get the kecamatan related to the penyuluh.
+     */
+    public function kecamatan()
+    {
+        return $this->belongsTo(MtKecamatan::class, 'tugas_kecamatan', 'id_kecamatan');
+    }
+}
