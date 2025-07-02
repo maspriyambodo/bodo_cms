ec53bbc8afab4b705469e8b93c56c5251bb2fb2f
LittleKatyusha <abiyusuf2809@gmail.com>
2025-07-02 23:49:27 +0700

    controller dan route bank

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\BankController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,4 @@ Route::middleware(['throttle:60,1'])->get('/ping', function () {
     return response()->json(['message' => 'Pong!']);
 });
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/banks', [BankController::class, 'index']);
