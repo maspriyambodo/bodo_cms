a9736db1bab9ce989c671881229ad2b52705fa25
priyambodo <maspriyambodo@gmail.com>
2025-07-03 00:58:53 +0700

    update database seeder

--- database/seeders/DtSubdirektoratSeeder.php
+++ database/seeders/DtSubdirektoratSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DtSubdirektoratSeeder extends Seeder
@@ -12,6 +12,188 @@ class DtSubdirektoratSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $subdirektorat = [
+            [
+                "id" => 1,
+                "id_direktorat" => 1,
+                "nama" => "BAGIAN UMUM & BMN",
+                "is_trash" => 0,
+                "created_at" => "2025-7-2 22 =>31 =>38",
+                "updated_at" => "2025-7-2 22 =>31 =>42",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 2,
+                "id_direktorat" => 2,
+                "nama" => "HISAB RUKYAT & SYARIAH",
+                "is_trash" => 0,
+                "created_at" => "2025-7-2 22 =>32 =>28",
+                "updated_at" => "2025-7-2 22 =>32 =>32",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 3,
+                "id_direktorat" => 3,
+                "nama" => "BINA KEPENGHULUAN",
+                "is_trash" => 0,
+                "created_at" => "2025-7-2 22 =>33 =>02",
+                "updated_at" => "2025-7-2 22 =>33 =>05",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 4,
+                "id_direktorat" => 4,
+                "nama" => "BINA PENYULUH AGAMA ISLAM",
+                "is_trash" => 0,
+                "created_at" => "2025-7-2 22 =>33 =>34",
+                "updated_at" => "2025-7-2 22 =>33 =>38",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 5,
+                "id_direktorat" => 5,
+                "nama" => "PERIZINAN & EVALUASI LEMBAGA ZAKAT dan WAKAF",
+                "is_trash" => 0,
+                "created_at" => "2025-7-2 22 =>34 =>26",
+                "updated_at" => "2025-7-2 22 =>34 =>30",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 6,
+                "id_direktorat" => 2,
+                "nama" => "KEMASJIDAN",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>46 =>12",
+                "updated_at" => "2025-7-3 00 =>46 =>15",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 7,
+                "id_direktorat" => 2,
+                "nama" => "BINA PAHAM KEAGAMAAN ISLAM & PENANGANAN KONFLIK KEAGAMAAN",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>46 =>59",
+                "updated_at" => "2025-7-3 00 =>47 =>02",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 8,
+                "id_direktorat" => 2,
+                "nama" => "KEPUSTAKAAN ISLAM",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>47 =>18",
+                "updated_at" => "2025-7-3 00 =>47 =>21",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 9,
+                "id_direktorat" => 3,
+                "nama" => "BINA KELEMBAGAAN & MUTU LAYANAN KUA",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>47 =>54",
+                "updated_at" => "2025-7-3 00 =>47 =>57",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 10,
+                "id_direktorat" => 3,
+                "nama" => "SARANA & PRASARANA KUA",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>48 =>21",
+                "updated_at" => "2025-7-3 00 =>48 =>24",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 11,
+                "id_direktorat" => 3,
+                "nama" => "BINA KELUARGA SAKINAH",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>48 =>42",
+                "updated_at" => "2025-7-3 00 =>48 =>45",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 12,
+                "id_direktorat" => 4,
+                "nama" => "DAKWAH & HARI BESAR ISLAM",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>49 =>11",
+                "updated_at" => "2025-7-3 00 =>49 =>14",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 13,
+                "id_direktorat" => 4,
+                "nama" => "LEMBAGA TILAWAH & MUSABAQAH ALQURAN",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>49 =>44",
+                "updated_at" => "2025-7-3 00 =>49 =>47",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 14,
+                "id_direktorat" => 4,
+                "nama" => "KEMITRAAN UMAT ISLAM",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>50 =>02",
+                "updated_at" => "2025-7-3 00 =>50 =>05",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 15,
+                "id_direktorat" => 4,
+                "nama" => "SENI BUDAYA & SIARAN KEAGAMAAN ISLAM",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>50 =>27",
+                "updated_at" => "2025-7-3 00 =>50 =>34",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 16,
+                "id_direktorat" => 5,
+                "nama" => "BINA KELEMBAGAAN & KERJASAMA ZAKAT WAKAF",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>51 =>10",
+                "updated_at" => "2025-7-3 00 =>51 =>13",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 17,
+                "id_direktorat" => 5,
+                "nama" => "PENGAWASAN LEMBAGA PENGELOLA ZAKAT",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>51 =>50",
+                "updated_at" => "2025-7-3 00 =>51 =>52",
+                "created_by" => 1,
+                "updated_by" => 1
+            ],
+            [
+                "id" => 18,
+                "id_direktorat" => 5,
+                "nama" => "PENGAWASAN & PENGAMANAN HARTA BENDA WAKAF",
+                "is_trash" => 0,
+                "created_at" => "2025-7-3 00 =>52 =>23",
+                "updated_at" => "2025-7-3 00 =>52 =>26",
+                "created_by" => 1,
+                "updated_by" => 1
+            ]
+        ];
+        DB::table('dt_subdirektorat')->insert($subdirektorat);
     }
 }
