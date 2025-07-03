75c6d41c851b93d803ab3f6cdafd04c7dfb3f2f7
priyambodo <maspriyambodo@gmail.com>
2025-07-03 23:32:14 +0700

    add module bank

---
+++ resources/views/nmbank/table_bank.blade.php
@@ -0,0 +1,108 @@
+<script>
+    var dt;
+    var KTDatatablesServerSide = function () {
+        var initDatatable = function () {
+            var dt = $('#table-bank').DataTable({
+                searchDelay: 500,
+                serverSide: true,
+                paging: true,
+                deferRender: true,
+                info: true,
+                stateSave: false,
+                initComplete: function () {
+                    Swal.close();
+                },
+                ajax: {
+                    url: "{{ route('nama-bank.json') }}",
+                    data: function (d) {
+                        d.keyword = $("#keyword").val();
+                    }
+                },
+                columnDefs: [
+                    {
+                        orderable: false, targets: [1,2,4]
+                    }
+                ],
+                columns: [
+                    {
+
+                        className: "text-center",
+                        render: function (data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    }, 
+                    {
+                        data: "button",
+                        className: "text-center"
+
+                    },
+                    {
+                        data: "nama"
+                    },
+                    {
+                        data: "kode"
+                    },
+                    {
+                        data: "logo",
+                        className: 'text-center'
+                    },
+                    {
+                        data: "status_aktif"
+                    }
+                ],
+                displayStart: 0,
+                pageLength: 10,
+                preDrawCallback: function () {
+                    Swal.fire({
+                        title: 'memuat data...',
+                        html: '<img src="{{ asset("src/media/misc/loading.gif") }}" title="Sedang Diverifikasi">',
+                        allowOutsideClick: false,
+                        showConfirmButton: false,
+                        onOpen: function () {
+                            Swal.showLoading();
+                        }
+                    });
+                },
+                rowCallback: function (row, data) {
+                    $('.dataTables_length').remove();
+                    $(row).addClass('border');
+                    Swal.close();
+                }
+            });
+            $('#_10').on('click', function () {
+                dt.page.len(10).draw();
+            });
+            $('#_25').on('click', function () {
+                dt.page.len(25).draw();
+            });
+            $('#_50').on('click', function () {
+                dt.page.len(50).draw();
+            });
+            $('#_100').on('click', function () {
+                dt.page.len(100).draw();
+            });
+            $('#dt_reload').on('click', function () {
+                $('#table-bank').DataTable().ajax.reload();
+            });
+            $('#keyword').on('keyup', function () {
+                var keyword = $('#keyword').val();
+                if (keyword.length >= 3) {
+                    dt.search(this.value).draw();
+                } else if (keyword == '') {
+                    dt.search(this.value).draw();
+                }
+            });
+            dt.on('draw', function () {
+                KTMenu.createInstances();
+            });
+        };
+        return {
+            init: function () {
+                initDatatable();
+            }
+        };
+    }();
+    KTUtil.onDOMContentLoaded(function () {
+        KTDatatablesServerSide.init();
+    });
+</script>
