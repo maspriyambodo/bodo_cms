ab992e7356796716a57e7d7eaf223927edaf94db
priyambodo <maspriyambodo@gmail.com>
2025-07-06 03:12:37 +0700

    update models

---
+++ app/Models/DetailKegiatan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetailKegiatan extends Model
+{
+    // Specify the table name if it doesn't follow Laravel's naming convention
+    protected $table = 'detail_kegiatan';
+}
---
+++ app/Models/DtKegiatan.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DtKegiatan extends Model
+{
+    // Specify the table name if it doesn't follow Laravel's naming convention
+    protected $table = 'dt_kegiatan';
+
+    // Primary key is 'id' by default, so no need to specify unless different
+    // Auto-incrementing primary key by default
+
+    // Disable timestamps if your table doesn't have Laravel's default timestamps
+
+    // Fillable fields for mass assignment
+    protected $fillable = [
+        'nama',
+        'direktorat',
+        'tanggal_mulai_kegiatan',
+        'tanggal_selesai_kegiatan',
+        'provinsi',
+        'kabupaten',
+        'kecamatan',
+        'kelurahan',
+        'lokasi_acara',
+        'link_biodata',
+        'is_trash',
+        'created_by',
+        'updated_by',
+    ];
+
+    // Cast attributes to appropriate types
+    protected $casts = [
+        'id' => 'integer',
+        'direktorat' => 'integer',
+        'tanggal_mulai_kegiatan' => 'date',
+        'tanggal_selesai_kegiatan' => 'date',
+        'provinsi' => 'integer',
+        'kabupaten' => 'integer',
+        'kecamatan' => 'integer',
+        'kelurahan' => 'integer',
+        'is_trash' => 'integer',
+        'created_at' => 'datetime',
+        'update_at' => 'datetime',
+        'created_by' => 'integer',
+        'updated_by' => 'integer',
+    ];
+}
