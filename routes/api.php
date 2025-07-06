9621fa6e7896119fd5d9d0a5de92de9773151b07
priyambodo <maspriyambodo@gmail.com>
2025-07-06 20:53:21 +0700

    update API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\BankController;
+use App\Http\Controllers\KegiatanController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,4 @@ Route::middleware(['throttle:60,1'])->get('/ping', function () {
 });
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/banks', [BankController::class, 'index']);
+Route::get('/kegiatan/{nama}', [KegiatanController::class, 'detailKegiatan']);
